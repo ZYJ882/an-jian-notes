17cd63ed9652fb7182ae8410054bc74992507f44
ZYJ882 <1.09709327e+08+ZYJ882@users.noreply.github.com>
2026-08-28 05:38:04 +0000

    移除剪贴板导入固定标题

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.anjiannotes"
         minSdk = 26
         targetSdk = 35
-        versionCode = 95
-        versionName = "2.6.51"
+        versionCode = 96
+        versionName = "2.6.52"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
